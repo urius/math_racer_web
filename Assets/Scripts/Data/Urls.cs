476b80221f7ff3d0ec0a3ee62efe5a2cf51df901
yuryisupov <isup89@gmail.com>
2025-02-18 14:13:20 +0300

    update default Url

--- Assets/Scripts/Data/Urls.cs
+++ Assets/Scripts/Data/Urls.cs
@@ -3,7 +3,7 @@ namespace Data
     public abstract class Urls
     {
         public static string P2PRoomsServiceUrl => $"{HostUrl}/math_racer/p2p_rooms/index.php";
-        public static string HostUrl { get; private set; } = "http://localhost";
+        public static string HostUrl { get; private set; } = "https://twin-pixel.ru";
 
         public static void SetHostUrl(string hostUrl)
         {
